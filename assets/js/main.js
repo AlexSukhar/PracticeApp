ee9d5c67cf16114b513c06aa816264fe7dc4b19a
AlexSukhar <suharikgt@gmail.com>
2022-06-14 19:25:50 +0300

    пока что так

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,61 @@
 const slide = document.querySelectorAll('.slide');
+const pauseButton = document.querySelector('#pause');
+const prevButton = document.querySelector('#prev');
+const nextButton = document.querySelector('#next');
+
 
 let currentSlide = 0;
+let isPlaying = true;
+let timerID = null;
+let interval = 1000
 
-function func() {
+function gotoNth(n) {
   slide[currentSlide].className = 'slide';
-  currentSlide = (currentSlide + 1) % 5
+  currentSlide = (n + 5) % 5;
   slide[currentSlide].className = 'slide active';
 }
 
+function prevSlide() {
+  gotoNth(currentSlide - 1)
+}
 
-setInterval(func,2000)
+function nextSlide() {
+  gotoNth(currentSlide + 1)
+}
+
+function nextAndPause() {
+  nextSlide();
+  onlyPause();
+}
+function prevAndPause() {
+  prevSlide();
+  onlyPause();
+}
+
+function onlyPause() {
+  if (isPlaying) {
+    clearInterval(timerID);
+    pauseButton.innerHTML = 'Play';
+    isPlaying = false;
+  }
+}
+
+function onlyPlay() {
+  timerID = setInterval(nextSlide, interval);
+  pauseButton.innerHTML = 'Pause';
+  isPlaying = true;
+}
+
+function pauseSlideshow() {
+  if (isPlaying) {
+   onlyPause();
+  } else {
+    onlyPlay();
+  }
+}
+
+pauseButton.addEventListener('click', pauseSlideshow);
+prevButton.addEventListener('click', prevAndPause);
+nextButton.addEventListener('click', nextAndPause);
+
+timerID = setInterval(gotoNext, interval);
