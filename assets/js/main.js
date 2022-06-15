4f80e1858c7244d603382943f0d7c2614d478214
AlexSukhar <suharikgt@gmail.com>
2022-06-15 11:18:08 +0300

    что -то сломалось

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 const slides = document.querySelectorAll('.slide');
+const indicatorsContainer = document.querySelector('#indicators-control');
+const indicators = document.querySelectorAll('.indicator');
 const pauseBtn = document.querySelector('#pause-btn');
 const prevBtn = document.querySelector('#prev-btn');
 const nextBtn = document.querySelector('#next-btn');
@@ -12,8 +14,10 @@ let interval = 2000;
 
 function gotoNth(n) {
   slides[currentSlide].classList.toggle('active');
+  indicators[currentSlide].classList.toggle('active');
   currentSlide = (n + slidesCount) % slidesCount;
   slides[currentSlide].classList.toggle('active');
+  indicators[currentSlide].classList.toggle('active');
 }
 
 const gotoPrev = () => gotoNth(currentSlide - 1);
@@ -45,9 +49,17 @@ function next() {
   pause();
 }
 
+function indicate(e) {
+  const target = e.target;
+  if (target && target.classList.contains('indicator')) {
+    pause();
+  gotoNth(+target.dataset.slideTo);
+  }
+}
 
 pauseBtn.addEventListener('click', pausePlay);
 prevBtn.addEventListener('click', prev);
 nextBtn.addEventListener('click', next);
+indicatorsContainer.addEventListener('click', indicate);
 
 timerID = setInterval(gotoNext, interval);
